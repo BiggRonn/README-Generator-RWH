c332fe466d9b88eb655298e69846c15db3224940
Wes Howard <BigRonald42@hotmail.com>
2021-03-18 20:32:43 -0700

    refined questions, add badge icons, full function

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const questions = [
     {
         type: "input",
         name: "installSteps",
-        message: "What are the steps required to install your project?"
+        message: "what is the command to install your project?"
     },
     {
         type: "input",
@@ -21,64 +21,29 @@ const questions = [
         message: "Provide instructions and examples for use"
     },
     {
-        type: "input",
-        name: "collab",
-        message: "List your collaborators"
-    },
-    {
         type: "list",
         name: "license",
         message: "Which license would you like to use?",
         choices: ["MIT","GNU","ISC","Apache"]
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+        
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+        
     }
 
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFile(fileName, `
-
-# ${data.title}
-
-## Description
- Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-## Table of Contents (Optional)
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-## Installation
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-${data.installSteps}
-## Usage
-Provide instructions and examples for use. Include screenshots as needed.
-To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-        md
-        ![alt text](assets/images/screenshot.png)
-        
-## Credits
-List your collaborators, if any, with links to their GitHub profiles.
-${data.collab}
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-If you followed tutorials, include links to those here as well.
-## License
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-${data.license}
----
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-## Badges
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-## Features
-If your project has a lot of features, list them here.
-## How to Contribute
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-## Tests
-`, (err) => console.log(err))
+    return fs.writeFile(fileName, generateMarkdown(data), (err) => (err) ? console.log(err) : console.log("Success!!"))
  }
 
 // TODO: Create a function to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,27 +1,83 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let retLicense = {
+    MIT : "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    ISC : "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+    GNP : "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    Apache: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+}
+
+  return retLicense[license]
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    let retLicense = "";
+  let retLicense = {
+    MIT : "Link to the MIT license........",
+    ISC : "Link to ISC license........",
+    GNP : "Link to the GNP license........",
+    Apache: "Link to the APACHE license........" 
+}
 
-    switch(license){
-        case "MIT": "awfe"
-        break;
-    }
-
-    return retLicense
+  return retLicense[license]
+    
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let retLicense = {
+    MIT : "This section will have information about the MIT license........",
+    ISC : "This section will have information about the ISC license........",
+    GNP : "This section will have information about the GNP license........",
+    Apache: "This section will have information about the APACHE license........"
+}
+
+  return retLicense[license]
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown({title, install, license, github, email}) {
+  return `
+  ${renderLicenseBadge(license)}
+
+  # ${title}
+
+  ## Description
+   Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+  - What was your motivation?
+  - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+  - What problem does it solve?
+  - What did you learn?
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  ## Installation
+  What is the command to install your project?
+  \`\`\`${install}\`\`\`
+  ## Usage
+  Provide instructions and examples for use. Include screenshots as needed.
+ 
+ 
+  ## License
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  ${renderLicenseSection(license)}
+  ${renderLicenseLink(license)}
+  ---
+
+  ## How to Contribute
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  ## Tests
+  
+  ##Questions
+  ${github}
+  ${email}
+
 
 `;
 }
